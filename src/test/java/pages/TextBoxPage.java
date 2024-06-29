eb5f579cb365612cbfac18a409da7ff8fdd7950e
Aleksandr Anosov <you@example.com>
2024-06-29 14:52:57 +0300

    fixed tests and pages imports and names

--- src/test/java/pages/TextBoxPage.java
+++ src/test/java/pages/TextBoxPage.java
@@ -9,4 +9,48 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxPage {
 
+    private final SelenideElement
+            userFullNameInput = $("#userName"),
+            emailInput = $("#userEmail"),
+            currentAddressInput = $("#currentAddress"),
+            permanentAddressInput = $("#permanentAddress"),
+            submit = $("#submit"),
+            resulTable = $("#output");
+
+    public TextBoxPage openPage() {
+        open("/text-box");
+        BannerDrop.bannerDrop();
+        return this;
+    }
+
+    public TextBoxPage setFullName(String userName) {
+        userFullNameInput.setValue(userName);
+        return this;
+    }
+
+    public TextBoxPage setEmail(String email) {
+        emailInput.setValue(email);
+        return this;
+    }
+
+    public TextBoxPage setCurrentAddress(String address) {
+        currentAddressInput.setValue(address);
+        return this;
+    }
+
+    public TextBoxPage setPermanentAddress(String address) {
+        permanentAddressInput.setValue(address);
+        return this;
+    }
+
+    public TextBoxPage submitClick() {
+        submit.click();
+        return this;
+    }
+
+    public TextBoxPage checkResult(String id, String text) {
+        resulTable.$(byId(id)).parent()
+                .shouldHave(text(text));
+        return this;
+    }
 }
