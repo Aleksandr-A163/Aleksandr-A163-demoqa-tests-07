9907ef3c98e3c98b64c2f2b0124c6c33b11ec2de
Aleksandr Anosov <you@example.com>
2024-06-29 14:19:59 +0300

    added file structure

---
+++ src/test/java/tests/WebFormTest.java
@@ -0,0 +1,65 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import com.codeborne.selenide.Selenide;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+
+public class WebFormTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+    }
+
+
+    @Test
+    void webFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript ("$('#fixedban').remove()");
+        executeJavaScript ("$('footer').remove()");
+
+        $("#firstName").setValue("Ahmed");
+        $("#lastName").setValue("Ahmedov");
+        $("#userEmail").setValue("testahmed@gmail.com");
+        $("#gender-radio-1+label").click();
+        $("#userNumber").setValue("7904744684");
+        $("#dateOfBirth #dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1985");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--001").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbies-checkbox-1+label").click();
+        $("#hobbies-checkbox-2+label").click();
+        $("#hobbies-checkbox-3+label").click();
+        $("#uploadPicture").uploadFromClasspath("leopard.png");
+        $("#currentAddress").setValue("Istanbul");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
+        $("#submit").click();
+
+        $("[class=table-responsive]").shouldHave(text("Ahmed Ahmedov"));
+        $("[class=table-responsive]").shouldHave(text("testahmed@gmail.com"));
+        $("[class=table-responsive]").shouldHave(text("Male"));
+        $("[class=table-responsive]").shouldHave(text("7904744684"));
+        $("[class=table-responsive]").shouldHave(text("01 April,1985"));
+        $("[class=table-responsive]").shouldHave(text("Maths"));
+        $("[class=table-responsive]").shouldHave(text("Sports, Reading, Music"));
+        $("[class=table-responsive]").shouldHave(text("leopard.png"));
+        $("[class=table-responsive]").shouldHave(text("Istanbul"));
+        $("[class=table-responsive]").shouldHave(text("Haryana Karnal"));
+    }
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
+    }
+}
